2ece78e036c9e3561ff60f701e4eedda808f3649
Aaron Rodriguez <aaronerdev@gmail.com>
2025-09-04 23:40:00 -0500

    creating routes

---
+++ app/routes/projects/index.tsx
@@ -0,0 +1,9 @@
+const ProjectPage = () => {
+  return (
+    <section>
+      <h2 className='text-3xl text-white font-bold mb-8'>🚀 Projects</h2>
+    </section>
+  );
+};
+
+export default ProjectPage;
